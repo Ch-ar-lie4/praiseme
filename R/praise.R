6fce0368c6a401d42ff5e65c98045346b42d1bb8
Charlie Recchia <charles.recchia@students.mq.edu.au>
2024-09-01 19:52:52 +1000

    Initial commit

---
+++ R/praise.R
@@ -0,0 +1,13 @@
+praise <- function(name, punctuation = "!"){
+  glue::glue("You're the best, {name}{punctuation}")
+}
+usethis::use_mit_license("Charlie Recchia")
+
+devtools::document()
+?praise
+#This is very broken no clue why
+
+usethis::use_package("glue")
+
+usethis::use_testthat()
+usethis::use_test("praise")
